24891a2e8988aee835a6c9a4a61be27087cf68c7
ilanvys <ilanvys@gmail.com>
2023-10-23 13:32:16 +0300

    First Working Version With  SearchBox

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import CardList from './CardList';
+import SearchBox from "./SearchBox";
+import { robots } from "./robots";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+	constructor() {
+		super();
+		this.state = {
+			robots: robots,
+			searchfieald: ''
+		}
+	}
+
+	onSearchUpdtae = (event) => {
+		this.setState({ searchfieald: event.target.value })
+	}
+
+	render() {
+		const filteredRobots = this.state.robots.filter(robot => {
+			return robot.name.toLowerCase().includes(this.state.searchfieald.toLowerCase())
+		})
+		
+		return (
+			<div className='tc'>
+				<h1>RoboFriends</h1>
+				<SearchBox 
+					searchUpdate={this.onSearchUpdtae}
+					searchfieald={this.state.searchfieald}
+				/>
+				<CardList robots={filteredRobots} />
+			</div>
+		)
+	}
 }
-
-export default App;
+	
+export default App
