c4011321ac305314581e1c89bc2d569db2e9b5b5
ilanvys <ilanvys@gmail.com>
2023-11-06 12:47:42 +0200

    Add Redux to the Project

--- src/index.js
+++ src/index.js
@@ -1,15 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { legacy_createStore as createStore,
+   applyMiddleware, 
+   combineReducers } from 'redux';
+import { createLogger } from 'redux-logger';
+import thunk from 'redux-thunk';
+
 import 'tachyons';
 import './index.css';
 import App from './containers/App';
 import reportWebVitals from './reportWebVitals';
+import { searchRobots, requestRobots } from './reducers';
+
+
+const logger = createLogger();
+const rootRrducer = combineReducers({ searchRobots, requestRobots })
+const store = createStore(rootRrducer, applyMiddleware(thunk, logger));
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
